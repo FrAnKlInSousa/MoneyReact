80bbe9d6fd08dfee8bb6e1ab3b5e98b4c14af0a2
franklin <frank.fkn.23@gmail.com>
2022-10-23 00:46:42 -0300

    criando o header

--- money/src/styles/global.ts
+++ money/src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
         --text-title: #363f5f;
         --text-body: #969cb3;
         --background: #f0f2f5;
-        
+
         --shape: #ffffff;
     }
     
@@ -27,9 +27,18 @@ export const GlobalStyle = createGlobalStyle`
             font-size: 87.5%;
         }
     }
+
     body{
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+    body, input, textarea, button{
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
     }
 
     button{
